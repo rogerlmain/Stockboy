952752cd98dcff369bb4dd74ed964ef6f9e64ec2
rexthestrange <roger.main@rexthestrange.com>
2024-04-15 15:42:31 -0600

    Refining holdings filter - updated share count

--- Database/create_function.share_count.sql
+++ Database/create_function.share_count.sql
@@ -1,13 +1,17 @@
 -- drop function share_count;
 
-create function share_count (@ticker_id uniqueidentifier = null) returns decimal as begin
+create function share_count (@ticker_id uniqueidentifier, @broker_id uniqueidentifier = null) returns decimal(14,6) as begin
 
 	declare @result decimal;
 
-	if @ticker_id is null
-		select @result = sum (quantity) from purchases
-	else
-		select @result = sum (quantity) from purchases where ticker_id = @ticker_id;
+	with quantities as (
+		select
+			(select sum(spp.split_quantity) from split_purchases () as spp where (spp.ticker_id = @ticker_id) and ((spp.broker_id = @broker_id) or (@broker_id is null))) as total_purchases,
+			(select sum(sps.split_quantity) from split_sales () as sps where (sps.ticker_id = @ticker_id) and ((sps.broker_id = @broker_id) or (@broker_id is null))) as total_sales
+	) select
+		@result = qty.total_purchases - coalesce (qty.total_sales, 0) -- as holdings
+	from
+		quantities as qty;
 
 	return @result;
 
